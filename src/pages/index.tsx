4b9584b1d365d24547c14fced989b5ead4252cd3
raynerljm <raynerljm@gmail.com>
2023-05-12 17:51:22 +0800

    fix: fix ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,10 +15,10 @@ const Home: NextPage = () => {
     <main className="h-screen grid place-items-center">
       <div className="bg-white rounded-md py-12 px-8 flex flex-col max-w-lg">
         <div className="flex gap-12 w-4/5 mx-auto mb-8">
-          <div className="w-full grid place-items-">
+          <div className="w-full grid place-items-center">
             <Image src={sgidLogo} alt="sgID logo" />
           </div>
-          <div className="w-full grid place-items-center relative">
+          <div className="w-full grid place-items-center">
             <Image src={singpassLogo} alt="Singpass logo" />
           </div>
         </div>
