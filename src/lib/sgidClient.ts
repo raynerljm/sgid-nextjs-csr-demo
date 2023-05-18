331ec97af142fc0745f364b631a90542c7c600e5
raynerljm <raynerljm@gmail.com>
2023-05-18 11:22:29 +0800

    feat: remove init of sdk

--- src/lib/sgidClient.ts
+++ src/lib/sgidClient.ts
@@ -1,13 +1,5 @@
 import { SgidClient } from "@opengovsg/sgid-client";
 
-const sgidClient = new SgidClient({
-  clientId: String(process.env.SGID_CLIENT_ID),
-  clientSecret: String(process.env.SGID_CLIENT_SECRET),
-  privateKey: String(process.env.SGID_PRIVATE_KEY),
-  redirectUri:
-    process.env.NODE_ENV !== "development"
-      ? "https://sgid-nextjs-csr-demo.vercel.app/api/callback"
-      : "http://localhost:3000/api/callback",
-});
+let sgidClient;
 
 export { sgidClient };
